04ca14dc9ec72e9ecb0c3db9257ef5bda7f63cc2
AI_Nomads <chugue85@gmail.com>
2024-02-07 11:43:43 +0900

    update done

--- src/main/java/shop/mtcoding/blog/board/BoardRepository.java
+++ src/main/java/shop/mtcoding/blog/board/BoardRepository.java
@@ -70,4 +70,13 @@ public class BoardRepository {
         Board board = (Board) query.getSingleResult();
         return board;
     }
+
+    @Transactional
+    public void update(BoardRequest.UpdateDTO requestDTO, int id) {
+        Query query = em.createNativeQuery("update board_tb set title=? , content=? where id = ? ");
+        query.setParameter(1, requestDTO.getTitle());
+        query.setParameter(2, requestDTO.getContent());
+        query.setParameter(3, id);
+        query.executeUpdate();
+    }
 }
